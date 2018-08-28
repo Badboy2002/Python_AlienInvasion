c13b7f637716e4e0e704a29e83e2c2a35d4d30ef
Badboy2002 <30972843+Badboy2002@users.noreply.github.com>
2018-08-28 10:50:17 +0800

    moving,bullet completed

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,8 +2,11 @@ import sys
 
 import pygame
 
+from pygame.sprite import Group
+
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -16,22 +19,22 @@ def run_game():
     bg_color = (ai_settings.bg_color)
 
     # 创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
+
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
 
     # 开始游戏的主循环
     while True:
 
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-            # 每次循环是重绘屏幕
-            screen.fill(bg_color)
-            ship.blitme()
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
-
+        gf.check_events(ai_settings, screen, ship, bullets)
+        # 更新飞船
+        ship.update()
+        # 更新子弹
+        bullets.update()
+        # 绘制屏幕
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
 
